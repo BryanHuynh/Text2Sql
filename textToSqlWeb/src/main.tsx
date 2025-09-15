914c31a8b7951dfc612332a1217b767e2022c57f
Bryan Huynh <32778577+BryanHuynh@users.noreply.github.com>
2025-09-14 20:36:56 -0600

    Revert "Feature/code panel"

--- textToSqlWeb/src/main.tsx
+++ textToSqlWeb/src/main.tsx
@@ -1,20 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-import { StyledEngineProvider } from "@mui/material";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./store";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-		<StyledEngineProvider injectFirst>
-			<Provider store={store}>
-				<BrowserRouter>
-					<App />
-				</BrowserRouter>
-			</Provider>
-		</StyledEngineProvider>
-	</StrictMode>
-);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
